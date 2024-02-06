6e8e5a1a516d11782d22b301558a8686c57a9d02
Yusuf Kaan SARI <yusufkaansari@hotmail.com>
2024-02-06 22:30:15 +0300

    Update Waypoint.cs

--- Assets/Scripts/Utilities/Waypoint.cs
+++ Assets/Scripts/Utilities/Waypoint.cs
@@ -1,34 +1,33 @@
 using System.Collections.Generic;
 using UnityEngine;
-
-    public class Waypoint : MonoBehaviour
+[DefaultExecutionOrder(-300)]
+public class Waypoint : MonoBehaviour
+{
+    public static List<Waypoint> Waypoints = new List<Waypoint>();
+    /* Use it:
+        if (Waypoint.TryGetWaypoint(out LevelManager levelManager)){_levelManager = levelManager;} 
+    */
+    private void OnEnable()
     {
-        public static List<Waypoint> Waypoints = new List<Waypoint>();
-        /* Use it:
-            if (Waypoint.TryGetWaypoint(out LevelManager levelManager)){_levelManager = levelManager;} 
-        */
-
-        private void Awake()
-        {
-            Waypoints.Add(this);
-        }
-
-        private void OnDestroy()
-        {
-            Waypoints.Remove(this);
-        }
-
-        public static bool TryGetWaypoint<T>(out T component) where T : class
-        {
-            for (int i = 0; i < Waypoints.Count; i++)
-            {
-                if (Waypoints[i].gameObject.TryGetComponent(out T component1))
-                {
-                    component = component1;
-                    return true;
-                }
-            }
-            component = null;
-            return false;
-        }
+        Waypoints.Add(this);
     }
+
+    private void OnDisable()
+    {
+        Waypoints.Remove(this);
+    }
+
+    public static bool TryGetWaypoint<T>(out T component) where T : class
+    {
+        for (int i = 0; i < Waypoints.Count; i++)
+        {
+            if (Waypoints[i].gameObject.TryGetComponent(out T component1))
+            {
+                component = component1;
+                return true;
+            }
+        }
+        component = null;
+        return false;
+    }
+}
